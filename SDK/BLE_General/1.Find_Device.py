8538c685361758a4050b025b022a87718c6f43f4
IldaRon <de.ikar@yandex.ru>
2025-01-10 10:34:46 +0000

    Update 1.Find_Device.py

--- SDK/BLE_General/1.Find_Device.py
+++ SDK/BLE_General/1.Find_Device.py
@@ -1,5 +1,11 @@
 import asyncio
 from bleak import BleakScanner
+import sys
+from scipy import signal
+#from matplotlib import pyplot as plt
+
+address = None
+FIRST_NAME_ID = '0000fe42-8e22-4541-9d4c-21edae82ed19'
 
 async def find_eareeg():
     devices = await BleakScanner.discover()
@@ -8,15 +14,16 @@ async def find_eareeg():
             print(f"Found EAREEG device:")
             print(f"Name: {device.name}")
             print(f"Address: {device.address}")
-            return device
+            address = device.address
+            
+            return address
     print("EAREEG device not found")
     return None
 
 async def main():
-    eareeg_device = await find_eareeg()
-    if eareeg_device:
-        # Add any additional actions you want to perform with the device here
-        pass
-
+    ironbci_address = await find_eareeg()
+    
+    if ironbci_address:
+        print ("ironbci_address", ironbci_address)    
+     
 asyncio.run(main())
-

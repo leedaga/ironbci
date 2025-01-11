4eb799fbce42b42b6b507b0ac69214496dbd3c0b
IldaRon <de.ikar@yandex.ru>
2025-01-11 00:43:32 +0000

    Update 2.Read_data.py

--- SDK/BLE_General/2.Read_data.py
+++ SDK/BLE_General/2.Read_data.py
@@ -6,14 +6,30 @@ import time
 FIRST_NAME_ID = '0000fe42-8e22-4541-9d4c-21edae82ed19'
 address = "00:80:E1:26:01:09"
 
+
+data_test= 0x7FFFFF
+data_check=0xFFFFFF
+
+
 async def main(address):
     async with BleakClient(address) as client:
         event = asyncio.Event()  # Create the event inside the coroutine
 
         def callback(FIRST_NAME_ID, data):
             #print(data[0], data[1], data[2])
-            rawData = (data[0] << 16) | (data[1] << 8) | data[2]
-            print(rawData)
+            voltage_1 = (data[0] << 16) | (data[1] << 8) | data[2]
+
+           
+            convert_voktage=voltage_1|data_test
+            
+            if convert_voktage==data_check:
+                voltage_1_after_convert=(voltage_1-16777214)
+            else:
+                voltage_1_after_convert=voltage_1
+            result=round(1000000*4.5*(voltage_1_after_convert/16777215),2)
+            print(result)
+
+            
             event.set()  # Set the event when data is received
 
         await client.start_notify(FIRST_NAME_ID, callback)
